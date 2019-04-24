7c49d898b8f7027ede32ae03563cd599c34c9688
jvgiv <jvgiv12@gmail.com>
2019-04-24 18:51:37 -0400

    All of user functions work except for get Posts

--- users/users-router.js
+++ users/users-router.js
@@ -33,12 +33,42 @@ router.get('/:id', async (req, res) => {
 // router.get()
 
 
-// router.post()
+router.post('/', async (req, res) => {
+    try {
+        const users = await Users.insert(req.body)
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Couldn't add the user"
+        })
+    }
+})
 
 
-// router.put()
+router.put('/:id', async (req, res) => {
+    try {
+        const users = await Users.update(req.params.id, req.body);
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Couldn't make an edit"
+        })
+    }
+})
 
 
-// router.delete()
+router.delete('/:id', async (req, res) => {
+    try {
+        const users = await Users.remove(req.params.id);
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Could not Delete'
+        })
+    }
+})
 
 module.exports = router
